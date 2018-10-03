212b65622d50dc50a756a061eb414d101e0e39bf
Duncan McGregor <duncan@oneeyedmen.com>
2018-10-03 09:56:59 +0200

    2nd commit of the day !

---
+++ exercise/learnyouakotlin/part1/Session.kt
@@ -0,0 +1,23 @@
+package learnyouakotlin.part1
+
+
+data class Session(val title: String, val subtitle: String?, val slots: Slots, val presenters: List<Presenter>) {
+
+    constructor(title: String, subtitle: String?, slots: Slots, vararg presenters: Presenter) : this(
+        title,
+        subtitle,
+        slots,
+        presenters.toList())
+
+    fun withPresenters(newLineUp: List<Presenter>): Session {
+        return Session(title, subtitle, slots, newLineUp)
+    }
+
+    fun withTitle(newTitle: String): Session {
+        return Session(newTitle, subtitle, slots, presenters)
+    }
+
+    fun withSubtitle(newSubtitle: String?): Session {
+        return Session(title, newSubtitle, slots, presenters)
+    }
+}
